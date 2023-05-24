ef04db695c8262cf7423ed64f964bab8d75f0368
lihang <2291732295@qq.com>
2023-05-24 23:28:19 +0800

    提交暂存

--- react_extension/src/App.js
+++ react_extension/src/App.js
@@ -1,13 +1,12 @@
 import React, { Component, lazy, Suspense } from 'react'
-import { Route, Switch, Redirect } from 'react-router-dom'
-import mock from './mock/index.js'
+import { Route, Switch } from 'react-router-dom'
 const Login = lazy(() => import('./pages/login/login.jsx'))
 const Admin = lazy(() => import('./pages/admin/admin.jsx'))
 
 export default class App extends Component {
   render() {
     return (
-      <Suspense fallback={<div>loading...</div>}>
+      <Suspense fallback={<div>储藏loading...</div>}>
         <Switch>
           <Route path="/login" component={Login}></Route>
           <Route path="/" component={Admin}></Route>
